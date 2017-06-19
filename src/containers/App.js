589aee8af5b0ab32657bdd01035b5cb0086790cb
Vuong Vu Nguyen <vuongvu333@gmail.com>
2017-06-19 22:46:10 +0700

    the-road-to-learn-react => complete chapter 2

--- src/containers/App.js
+++ src/containers/App.js
@@ -22,51 +22,72 @@ const list = [
 
 const isSearched = (searchTerm) => (item) => !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-
-    return (
-      <form>
-        {children} <input 
-          type='text'
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-    );
-  }
+const Search = ({ value, onChange, children }) => {
+  return (
+    <form>
+      {children} <input 
+        type='text'
+        value={value}
+        onChange={onChange}
+      />
+    </form>
+  );
 }
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
+const Button = ({ onClick, className, children }) => {
+  return (
+    <button
+      type='button'
+      className={className}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
 
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map((item) => {
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                  type='button'
-                  onClick={() => onDismiss(item.objectID)}
-                >
-                  Dismiss
-                </button>
-              </span>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
+const Table = ({ list, pattern, onDismiss }) => {
+  const largeColumn = {
+    width: '40%',
+  };
+
+  const midColumn = {
+    width: '30%',
+  };
+
+  const smallColumn = {
+    width: '10%',
+  };
+  return (
+    <div className='table'>
+      {list.filter(isSearched(pattern)).map((item) => {
+        return (
+          <div key={item.objectID} className='table-row'>
+            <span style={largeColumn}>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span style={midColumn}>
+              {item.author}
+            </span>
+            <span style={smallColumn}>
+              {item.num_comments}
+            </span>
+            <span style={smallColumn}>
+              {item.points}
+            </span>
+            <span style={smallColumn}>
+              <Button
+                className='button-inline'
+                onClick={() => onDismiss(item.objectID)}
+              >
+                Dismiss
+              </Button>
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 class App extends Component {
@@ -99,13 +120,15 @@ class App extends Component {
     const { list, searchTerm } = this.state;
 
     return (
-      <div className='container'> 
-        <Search 
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        >
-          Search
-        </Search>
+      <div className='page'> 
+        <div className='interactions'>
+          <Search 
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
+          </Search>
+        </div>
         <Table 
           list={list}
           pattern={searchTerm}
